5c864e91a778389b770fe27b3360534c18006a38
huisusu <yudasuhui@hotmail.com>
2017-12-13 22:36:34 +0100

    123

---
+++ www/js/classes/game.class.js
@@ -0,0 +1,7 @@
+class Game extends Base{
+    constructor(board, player1, player2){
+        this.board= board;
+        this.player1=player1;
+        this.player2=player2;
+    }
+}
--- www/js/main.js
+++ www/js/main.js
@@ -1,25 +1,9 @@
-function scale(){
-  let orgW = 1050, orgH = 900;
-  let w = $('.game-div').width();
-  let h = $(window).height();
-  // Adjust h for headers, margins etc
-  h -= $('#mainNav').outerHeight() + 50;
-  // w -= 20 * 2;
-  // This scaling would fit to width
-  let wScale = w / orgW;
-  // This scaling would fit to height
-  let hScale = h / orgH;
-  // This scaling would fit both width and height
-  let scaling = Math.min(wScale, hScale);
-  // Apply scaling
-  $('.board').css('transform', `scale(${scaling})`);
-  $('.board').show();
-  // Set the holder to the scaled width and height
-  $('.board-holder').width(orgW * scaling);
-  $('.board-holder').height(orgH * scaling);
-}
+
+let game;
+
+JSON._classes(Game, Board, Player, Computer);
 
 // Run on page load
-scale();
+board.scale();
 // Run every time the size changes
 $(window).resize(scale);
