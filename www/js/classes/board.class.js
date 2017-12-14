856e89a3d442d2ae1dada8ca0e2ec3685af73502
huisusu <yudasuhui@hotmail.com>
2017-12-14 23:16:03 +0100

    back up for player

--- www/js/classes/board.class.js
+++ www/js/classes/board.class.js
@@ -1,45 +1,10 @@
-class Board extends Base {
-    constructor() {
-        super();
-        this.gameField = [];
-        for (let i = 0; i < 7; i++) {
-            this.gameField.push([]);
-            for(let j=0; j<6;j++){
-                this.gameField[i].push(-1);            
-            }
-        }
-    }
+class Board(selector) {
 
-    render(el) {
-        super.render(el);
-    }
+  constructor() {
+    this.rows = 6;
+    this.cols = 7;
 
-    scale() {
-        let orgW = 1050, orgH = 900;
-        let w = $('.game-div').width();
-        let h = $(window).height();
-        // Adjust h for headers, margins etc
-        h -= $('#mainNav').outerHeight() + 50;
-        // w -= 20 * 2;
-        // This scaling would fit to width
-        let wScale = w / orgW;
-        // This scaling would fit to height
-        let hScale = h / orgH;
-        // This scaling would fit both width and height
-        let scaling = Math.min(wScale, hScale);
-        // Apply scaling
-        $('.board').css('transform', `scale(${scaling})`);
-        $('.board').show();
-        // Set the holder to the scaled width and height
-        $('.board-holder').width(orgW * scaling);
-        $('.board-holder').height(orgH * scaling);
-    }
+  }
 
-    makeMove() {
 
-    }
-
-    checkIfWin() {
-
-    }
 }
--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,46 @@
-//Use Jquery to load user profiles
-//Create a URL of start page with parameters
-//Example: localhost:3000/html/board.html?playerA=name,red,0&playerB=name,yellow,1,easy
+// test code for class Player
+$('.spela-btn').on('click', function(){
+  let player1 = new Player({name:"p1", color:"red", ai:true, difficulty:1});
+  let player2 = new Player({name:"p2", color:"yellow", ai:false, difficulty:2});
+  player1.render('.p1-info');
+  player2.render('.p2-info');
+});
+
+// test code for class Player
+
+
+
+let game;
+
+JSON._classes(Game, Board, Player);
+
+
+
+
+
+// Scaling function so the board fits the browser widnow
+function scale(){
+  let orgW = 1050, orgH = 900;
+  let w = $('.game-div').width();
+  let h = $(window).height();
+  // Adjust h for headers, margins etc
+  h -= $('#mainNav').outerHeight() + 50;
+  // This scaling would fit to width
+  let wScale = w / orgW;
+  // This scaling would fit to height
+  let hScale = h / orgH;
+  // This scaling would fit both width and height
+  let scaling = Math.min(wScale, hScale);
+  // Apply scaling
+  $('.board').css('transform', `scale(${scaling})`);
+  $('.board').show();
+  // Set the holder to the scaled width and height
+  $('.board-holder').width(orgW * scaling);
+  $('.board-holder').height(orgH * scaling);
+}
+
+// Run on page load
+scale();
+// Run every time the size changes
+$(window).resize(scale);
+
